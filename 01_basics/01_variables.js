e91725f7fe472d28a33a7077e0cd94e89f633c89
Krishna <kabirsaxena2007@gmail.com>
2025-08-01 08:15:18 +0000

    Added variables and datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountID = 123456
+let accountEMAIL = "krishnasaxena@gmail.com"
+var accountPASSWORD = "12345"
+accountCITY = "Mathura"
+let accountSTATE;
+
+accountEMAIL = "hc@hc.com"
+accountPASSWORD = "98765"
+accountCITY = "Agra"
+
+console.table([accountID, accountEMAIL, accountPASSWORD, accountCITY, accountSTATE])
